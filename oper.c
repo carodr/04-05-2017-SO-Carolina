c1aa4aba4298854f2ec3fc5ecdfd3c23593aa82d
ivanmtoroc <ivan.toro@correounivalle.edu.co>
2017-04-05 16:17:55 -0500

    primer

---
+++ oper.c
@@ -0,0 +1,25 @@
+#include "oper.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void suma(int* a, int b)
+{
+	*a = *a + b;
+	printf("%d\n",a);
+}
+
+void resta(int* a, int b)
+{
+	*a = *a - b;
+	printf("%d\n",a);
+}
+
+void sumav(int *a, int *b, int n)
+{
+	
+}
+
+void restav(int *a, int *b, int n)
+{
+
+}
